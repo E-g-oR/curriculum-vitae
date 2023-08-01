46d58589ed54a05a8977605d9466f90b7855f197
Egor Muzychkin <e.myzichkin@rit.va>
2023-08-01 16:13:46 +0300

    feature: add function to get dates years for time period

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,12 @@
 import type {Languages} from "./i18n/translation";
+import {getYear, isSameYear} from "date-fns";
 
 export const getLanguageFromURL = (pathname: string): Languages => {
     const langCodeMatch = pathname.match(/\/([a-z]{2}-?[a-z]{0,2})\//);
     return (langCodeMatch ? langCodeMatch[1] : 'en') as Languages;
 }
+
+export const getYears = ([start, end]: [Date, Date]) =>
+    isSameYear(start, end)
+        ? getYear(start)
+        : `${getYear(start)} – ${getYear(end)}`
